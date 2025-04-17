02d8f4eee529c81a92999360e34b66d5332b01ac
Bianka R <bianka_romero@me.com>
2025-04-18 00:26:36 +0200

    export named individual social icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,33 +1,52 @@
-const SocialIcons = () => {
-  return (
-    <div>
-    
-        
-          <a href="https://www.linkedin.com/in/biankalromero/" target="_blank">
-          <ion-icon name="logo-linkedin"></ion-icon>
-          </a>
-        
-        
-          <a href="https://github.com/Bianka2112" target="_blank">
-          <ion-icon name="logo-github"></ion-icon>
-          </a>
-        
-        
-          <a href="https://codepen.io/Bianka2112" target="_blank">
-          <ion-icon name="logo-codepen"></ion-icon>
-          </a>
-        
-        
-          <a href="https://www.instagram.com/biankashoots" target="_blank">
-          <ion-icon name="logo-instagram"></ion-icon>
-          </a>
-        
-      <ion-icon name="heart"></ion-icon>
-    
-      
-    </div>
-  )
 
+export const LinkedinIcon = () => {
+  return (
+  <a href="https://www.linkedin.com/in/biankalromero/" target="_blank">
+  <ion-icon name="logo-linkedin"></ion-icon>
+  </a>
+  )
+}
+
+export const GithubIcon = () => {
+  return (
+  <a href="https://github.com/Bianka2112" target="_blank">
+  <ion-icon name="logo-github"></ion-icon>
+  </a>
+  )
+}
+
+export const CodepenIcon = () => {
+  return (
+  <a href="https://codepen.io/Bianka2112" target="_blank">
+  <ion-icon name="logo-codepen"></ion-icon>
+  </a>
+  )
+}
+
+export const InstagramIcon = () => {
+  return (
+  <a href="https://www.instagram.com/biankashoots" target="_blank">
+  <ion-icon name="logo-instagram"></ion-icon>
+  </a>
+  )
+}
+
+export const HeartIcon = () => {
+  return (
+  <ion-icon name="heart"></ion-icon>
+  )
+}
+
+const SocialIcons = () => {
+return (
+  <div>
+    <LinkedinIcon />
+    <GithubIcon />
+    <CodepenIcon />
+    <InstagramIcon />
+    <HeartIcon />
+  </div>
+  )
 }
 
 export default SocialIcons
