e6fbcf7afc47a601564146114efad53777a4f697
Bianka R <bianka_romero@me.com>
2025-04-27 15:18:38 -0400

    format footer styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,19 @@
+import styled from "styled-components"
+
+const ContactContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 64px 0;
+`
+
 const Contact = ({ name, phone, email }) => {
   return (
-    <>
+    <ContactContainer>
       <p>{name}</p>
       <p>{phone}</p>
       <p>{email}</p>
-    </>
+    </ContactContainer>
   )
 }
 
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,16 +1,27 @@
 import Contact from "../components/Contact"
 import SocialIcons from "../components/SocialIcons"
 import styled from "styled-components"
+import BiankaRomero from "../assets/BiankaRomero.jpg"
 
 const FooterContainer = styled.footer`
-   text-align: center;
+  padding: 128px;
+  text-align: center;
+`
+const CircleImg = styled.img`
+  width: 164px;
+  height: 164px;
+  border-radius: 164px;
+  object-fit: cover;
+`
+const FooterTitle = styled.h3`
+  padding-bottom: 40px;
 `
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <h3>Let's Connect</h3>
-      
+      <FooterTitle>Let's Connect</FooterTitle>
+      <CircleImg src={BiankaRomero} alt="" />
       <Contact 
         name="Bianka L Romero"
         phone="+46(0) 76 716 97 46"
