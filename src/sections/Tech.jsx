31643bd88e0066c78d07d37bb4be98666a3ee1cb
Bianka R <bianka_romero@me.com>
2025-04-27 19:03:48 -0400

    font, media, tech sec

--- src/sections/Tech.jsx
+++ src/sections/Tech.jsx
@@ -7,9 +7,22 @@ const TechSection = styled.div`
   color: white;
   padding: 64px 16px;
   text-align: center;
+  width: 100%;
 
   @media ${media.desktop} {
     padding: 128px 0;
+  }
+`
+const TechTitle = styled.h2`
+  font-size: 60px;
+
+  @media ${media.desktop} {
+    font-size: 80px;
+  }
+`
+
+const CardContainer = styled.div`
+  @media ${media.desktop} {
     max-width: 782px;
     margin: 0 auto;
   }
@@ -18,10 +31,13 @@ const TechSection = styled.div`
 const Tech = () => {
   return (
     <TechSection>
-      <Card 
-        title="Tech"
-        text="HTML, CSS, Flexbox, JavaScript, ES6, React, React Hooks, Node.js,  Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. "
-        />
+      <CardContainer>
+        <Card 
+          TitleComponent={TechTitle}
+          title="Tech"
+          text="HTML, CSS, Flexbox, JavaScript, ES6, React, React Hooks, Node.js,  Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. "
+          />
+      </CardContainer>
     </TechSection>
   )
 }
