051091fb30d81a1a8dba43eff401923e93700900
Bianka R <bianka_romero@me.com>
2025-04-27 22:33:57 -0400

    hero imgs media q, finally fix mobile

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -5,53 +5,48 @@ import styled from "styled-components"
 import { media } from "../styles/media"
 
 const HeroContainer = styled.div`
-  /* scale: 50%; */
-  height: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+  /* height: auto; */
+  min-width: 300px;
   width: 100%;
+  display: flex;
+  justify-content: center;
   padding: 16px;
+  margin: 0 auto;
 
-  @media ${media.tablet} {
-    width: 80%;
+`
+const HeroImg = styled.img`
+  width: 30%; /* 🚨 About 1/3rd of container, adjust a bit for gaps */
+  height: auto;
+  object-fit: cover;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+
+  @media ${media.tablet}, ${media.desktop} {
+    width: 300px;
   }
 `
 
-const RightSideImg = styled.img`
-  right: 60%;
-  width: 300px;
-  height: auto;
-  object-fit: cover;
-  border-radius: 12px;
-  transform: rotate(4.7deg) translate(-50px);
-  z-index: -1;
-`
-const LeftSideImg = styled.img`
-  left: 60%;
-  width: 300px;
-  height: auto;
-  object-fit: cover;
-  border-radius: 12px;
+const LeftHeroImg = styled(HeroImg)`
   transform: rotate(-4.7deg) translate(50px);
   z-index: -1;
 `
 
-const MainImg = styled.img`
-  width: 300px;
-  height: auto;
-  object-fit: cover;
-  border-radius: 12px;
-  transform: translate();
+const CenterHeroImg = styled(HeroImg)`
   z-index: 0;
 `
+
+const RightHeroImg = styled(HeroImg)`
+  transform: rotate(4.7deg) translate(-50px);
+  z-index: -1;
+`
+
 const HeroImages = () => {
-return (
-  <HeroContainer>
-    <LeftSideImg src={laptopBG} alt="" />
-    <MainImg src={BiankaRomero} alt="image of Bianka Romero" />
-    <RightSideImg src={cameraBG} alt="" />
-  </HeroContainer>
+  return (
+    <HeroContainer>
+      <LeftHeroImg src={laptopBG} alt="Laptop background" />
+      <CenterHeroImg src={BiankaRomero} alt="Bianka Romero" />
+      <RightHeroImg src={cameraBG} alt="Camera background" />
+    </HeroContainer>
   )
 }
 
--- src/styles/media.js
+++ src/styles/media.js
@@ -1,5 +1,5 @@
 export const media = {
-  mobile: "(max-width: 450px)",
-  tablet: "(max-width: 1023px)",
+  mobile: "(max-width: 490px)",
+  tablet: "(max-width: 768px)",
   desktop: "(min-width: 1024px)"
 }
